edde131e8e85d90b7ca3677d84c18d81c20b71f8
Jeremy Jean-Jean <jeremy.jean-jean@swerea.se>
2021-03-04 16:21:15 +0100

    consummer

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -5,11 +5,13 @@ import confluent_kafka
 from confluent_kafka import Consumer
 from confluent_kafka.avro import AvroConsumer
 from confluent_kafka.avro.serializer import SerializerError
+from confluent_kafka.cimpl import OFFSET_BEGINNING
 from tornado import gen
 
 
 logger = logging.getLogger(__name__)
-
+BROKER_URL = "PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
 
 class KafkaConsumer:
     """Defines the base kafka consumer class"""
@@ -30,33 +32,29 @@ class KafkaConsumer:
         self.consume_timeout = consume_timeout
         self.offset_earliest = offset_earliest
 
-        #
-        #
         # TODO: Configure the broker properties below. Make sure to reference the project README
         # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
+
         self.broker_properties = {
-                #
-                # TODO
-                #
+            "bootstrap.servers": BROKER_URL,
+            "schema.registry.url": SCHEMA_REGISTRY_URL,
+            "group.id": "0", 
+            'default.topic.config': {'auto.offset.reset': 'earliest'}
         }
 
         # TODO: Create the Consumer, using the appropriate type.
         if is_avro is True:
             self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
+            self.consumer = AvroConsumer(self.broker_properties)
+            
         else:
-            #self.consumer = Consumer(...)
+            self.consumer = Consumer(self.broker_properties)
             pass
 
-        #
-        #
         # TODO: Configure the AvroConsumer and subscribe to the topics. Make sure to think about
         # how the `on_assign` callback should be invoked.
-        #
-        #
-        # self.consumer.subscribe( TODO )
+
+        self.consumer.subscribe([self.topic_name_pattern], on_assign = self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
@@ -64,12 +62,10 @@ class KafkaConsumer:
         # the beginning or earliest
         logger.info("on_assign is incomplete - skipping")
         for partition in partitions:
-            pass
-            #
-            #
+            if self.offset_earliest:
+                partition.offset = 'OFFSET_BEGINING'
+
             # TODO
-            #
-            #
 
         logger.info("partitions assigned for %s", self.topic_name_pattern)
         consumer.assign(partitions)
@@ -84,21 +80,32 @@ class KafkaConsumer:
 
     def _consume(self):
         """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
+
         # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
         # Additionally, make sure you return 1 when a message is processed, and 0 when no message
         # is retrieved.
-        #
-        #
+
+        try:
+            msg = self.consumer.poll(self.consume_timeout)
+        
+        except Exception as e:
+            logger.error(f'Poll Exception {e}')
+            return 0
+
+        if msg is None:
+            logger.debug('Message is empty')
+            return 0
+        elif msg.error() is not None:
+            logger.error(f'Message error {msg.error()}')
+        else:
+            self.message_handler(msg)
         logger.info("_consume is incomplete - skipping")
         return 0
 
 
     def close(self):
         """Cleans up any open kafka consumers"""
-        #
-        #
+
         # TODO: Cleanup the kafka consumer
-        #
-        #
+
+        self.consummer.close()
--- consumers/ksql.py
+++ consumers/ksql.py
@@ -23,14 +23,19 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name STRING,
+    line STRING
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'org.chicago.cta.turnstile',
+    VALUE_FORMAT = 'avro',
+    KEY = 'station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT = 'JSON') AS 
+    SELECT station_id, station_name, line, COUNT(station_id) AS count FROM turnstile
+    GROUP BY station_id, station_name, line;
 """
 
 
