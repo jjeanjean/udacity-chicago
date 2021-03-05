e22f7bf06e3ddc03509784518055c0ed01e34d76
Jeremy Jean-Jean <jeremy.jean-jean@swerea.se>
2021-03-05 10:08:40 +0100

    fix locally

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -34,8 +34,9 @@ CREATE TABLE turnstile (
 
 CREATE TABLE turnstile_summary
 WITH (VALUE_FORMAT = 'JSON') AS 
-    SELECT station_id, station_name, line, COUNT(station_id) AS count FROM turnstile
-    GROUP BY station_id, station_name, line;
+    SELECT station_id, COUNT(station_id) AS count 
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
